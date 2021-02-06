5a7f6eebd035ce43a06824d939ac28352b9503ad
vfurmane <vfurmane@student.42.fr>
2021-02-06 13:37:28 +0100

    Replace ret with other names

--- srcs/color.c
+++ srcs/color.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 11:44:04 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/02/06 12:08:48 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/02/06 13:34:40 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int	ft_parse_color(const char *str)
 {
 	int	i;
-	int	ret;
+	int	nbr;
 	int	color;
 
 	i = 0;
 	color = 0;
 	while (i < 3)
 	{
-		ret = ft_atoi(str);
+		nbr = ft_atoi(str);
 		color = color << 2 * 4;
-		color += ret;
+		color += nbr;
 		while (*str && *str >= '0' && *str <= '9')
 			str++;
 		if (*str == ',')
--- srcs/handle_scene.c
+++ srcs/handle_scene.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 09:47:22 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/02/06 10:08:14 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/02/06 13:36:29 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,34 @@
 
 void	ft_handle_resolution(char **args, t_scene *scene)
 {
-	int	ret;
+	int	nbr;
 
-	ret = atoi(args[0]);
-	if (ret < 0)
+	nbr = atoi(args[0]);
+	if (nbr < 0)
 		return ; /* return value */
-	scene->plan.width = ret;
-	ret = atoi(args[1]);
-	if (ret < 0)
+	scene->plan.width = nbr;
+	nbr = atoi(args[1]);
+	if (nbr < 0)
 		return ; /* return value */
-	scene->plan.height = ret;
+	scene->plan.height = nbr;
 }
 
 void	ft_handle_ambiant(char **args, t_scene *scene)
 {
-	double	ret;
+	double	nbr;
 
-	ret = ft_atof(args[0]);
-	if (ret < 0)
+	nbr = ft_atof(args[0]);
+	if (nbr < 0)
 		return ; /* return value */
-	ft_set_ambiant(scene, ret, args[1]);
+	ft_set_ambiant(scene, nbr, args[1]);
 }
 
 void	ft_handle_light(char **args, t_scene *scene)
 {
-	double	ret;
+	double	nbr;
 
-	ret = ft_atof(args[1]);
-	if (ret < 0)
+	nbr = ft_atof(args[1]);
+	if (nbr < 0)
 		return ; /* return value */
-	ft_lstadd_front((void**)&scene->bulbs, (void*)ft_new_light(args[0], ret, args[2]));
+	ft_lstadd_front((void**)&scene->bulbs, (void*)ft_new_light(args[0], nbr, args[2]));
 }
