9099956665b4065ea426a0f71c9b28842faa55fa
vfurmane <vfurmane@student.42.fr>
2021-02-06 12:26:03 +0100

    Parse double vectors

--- srcs/strto.c
+++ srcs/strto.c
@@ -6,11 +6,26 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 10:58:26 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/02/06 12:00:23 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/02/06 12:23:25 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
+
+int			ft_step_to_number(const char *str, int i)
+{
+	if (str[i] == '-')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (str[i] == '.')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+		i++;
+	if (str[i] == ',')
+		i++;
+	return (i);
+}
 
 t_vector	ft_str_to_vector(const char *str)
 {
@@ -18,24 +33,10 @@ t_vector	ft_str_to_vector(const char *str)
 	static t_vector	vector;
 
 	i = 0;
-	vector.x = ft_atoi(&str[i]);
-	if (str[i] == '-')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-		i++;
-	if (str[i] == ',')
-		i++;
-	vector.y = ft_atoi(&str[i]);
-	if (str[i] == '-')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-		i++;
-	if (str[i] == ',')
-		i++;
-	vector.z = ft_atoi(&str[i]);
-	if (str[i] == '-')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-		i++;
+	vector.x = ft_atof(&str[i]);
+	i = ft_step_to_number(str, i);
+	vector.y = ft_atof(&str[i]);
+	i = ft_step_to_number(str, i);
+	vector.z = ft_atof(&str[i]);
 	return (vector);
 }
