62a6ccb9c049d97845d9ba6605ede4cffc02a6f6
vfurmane <vfurmane@student.42.fr>
2021-03-10 14:21:53 +0100

    Check incomplete vectors

--- srcs/check_strto.c
+++ srcs/check_strto.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:55:26 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/02/28 19:54:03 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/03/10 14:18:57 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,31 @@ int			ft_check_str_to_vector(const char *str, int norm, int line)
 	int				i;
 
 	i = 0;
+	if (str[i] == '\0')
+	{
+		printf("Error\nline %d: Vector format must be [-1.0,1.0]\n", line);
+		return (0);
+	}
 	if (!ft_check_atof(&str[i], ",", line))
 		return (0);
 	if (norm && !ft_check_norm(&str[i], line))
 		return (0);
 	i = ft_step_to_number(str, i);
+	if (str[i] == '\0')
+	{
+		printf("Error\nline %d: Vector format must be [-1.0,1.0]\n", line);
+		return (0);
+	}
 	if (!ft_check_atof(&str[i], ",", line))
 		return (0);
 	if (norm && !ft_check_norm(&str[i], line))
 		return (0);
 	i = ft_step_to_number(str, i);
+	if (str[i] == '\0')
+	{
+		printf("Error\nline %d: Vector format must be [-1.0,1.0]\n", line);
+		return (0);
+	}
 	if (!ft_check_atof(&str[i], "", line))
 		return (0);
 	if (norm && !ft_check_norm(&str[i], line))
