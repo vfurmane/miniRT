7190e988d2eb5c9923e063716dd612004d53f6cf
vfurmane <vfurmane@student.42.fr>
2021-03-13 15:48:11 +0100

    Put the image back to the window when reduced

--- srcs/mlx_event.c
+++ srcs/mlx_event.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 10:03:13 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/03/13 15:38:30 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/03/13 15:47:27 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,16 @@ int		ft_handle_key(int code, t_scene *scene)
 	return (0);
 }
 
+int		ft_put_scene_back(t_scene *scene)
+{
+	mlx_put_image_to_window(scene->mlx, scene->win, scene->img->img, 0, 0);
+	return (0);
+}
+
 void	my_mlx_events(t_scene *scene)
 {
 	mlx_loop_hook(scene->mlx, ft_loop_hook, scene);
 	mlx_key_hook(scene->win, ft_handle_key, scene);
+	mlx_hook(scene->win, 15, 1L << 16, ft_put_scene_back, scene);
 	mlx_hook(scene->win, 33, 1L << 17, ft_close_window, scene);
 }
