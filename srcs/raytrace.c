621956e2e99dfc0431026d0ad4e8f8523f0f4d48
vfurmane <vfurmane@student.42.fr>
2021-03-19 11:10:20 +0100

    Check the intersections in a separate function

--- srcs/raytrace.c
+++ srcs/raytrace.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 13:06:16 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/03/19 10:06:00 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/03/19 11:10:01 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,37 +53,44 @@ double	ft_closest_obj(t_obj *obj1, t_obj *obj2)
 	return (-1);
 }
 
+void	ft_check_intersections(t_scene scene,
+		t_obj *closest_obj, t_obj *obj_copy, double inter[2])
+{
+	if (inter[0] >= scene.inter_min &&
+			(inter[0] <= scene.inter_max || scene.inter_max == -1) &&
+			(inter[0] < closest_obj->inter || closest_obj->inter == -1))
+	{
+		closest_obj->type = obj_copy->type;
+		closest_obj->ptr = obj_copy->ptr;
+		closest_obj->inter = inter[0];
+	}
+	if (inter[1] >= scene.inter_min &&
+			(inter[1] <= scene.inter_max || scene.inter_max == -1) &&
+			(inter[1] < closest_obj->inter || closest_obj->inter == -1))
+	{
+		closest_obj->type = obj_copy->type;
+		closest_obj->ptr = obj_copy->ptr;
+		closest_obj->inter = inter[1];
+	}
+}
+
 double	ft_closest_intersection(t_ray *ray,
 		t_scene scene, t_obj *obj)
 {
 	double	inter[2];
-	double	closest_inter;
 	t_obj	obj_copy;
 	t_obj	closest_obj;
 
 	obj_copy = *obj;
-	closest_inter = -1;
+	closest_obj.inter = -1;
 	while (obj_copy.ptr != NULL)
 	{
 		ft_intersect_ray(ray, obj_copy, inter);
-		if (inter[0] >= scene.inter_min &&
-				(inter[0] <= scene.inter_max || scene.inter_max == -1) &&
-				(inter[0] < closest_inter || closest_inter == -1))
-		{
-			closest_inter = inter[0];
-			closest_obj = obj_copy;
-		}
-		if (inter[1] >= scene.inter_min &&
-				(inter[1] <= scene.inter_max || scene.inter_max == -1) &&
-				(inter[1] < closest_inter || closest_inter == -1))
-		{
-			closest_inter = inter[1];
-			closest_obj = obj_copy;
-		}
+		ft_check_intersections(scene, &closest_obj, &obj_copy, inter);
 		obj_copy.ptr = ((t_basic_obj*)obj_copy.ptr)->next;
 	}
 	obj->ptr = closest_obj.ptr;
-	return (closest_inter);
+	return (closest_obj.inter);
 }
 
 int		ft_trace_ray(t_ray *ray, t_scene scene)
