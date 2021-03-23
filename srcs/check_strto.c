084f6103125a9fe2b66262d4e09b74a6d868b9cc
vfurmane <vfurmane@student.42.fr>
2021-03-23 09:22:36 +0100

    Display the error message once when a vector is not normalized

--- srcs/check_strto.c
+++ srcs/check_strto.c
@@ -6,19 +6,16 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 18:55:26 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/03/22 20:12:55 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/03/23 09:21:48 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
-static int	ft_check_norm(const char *str, int line)
+static int	ft_check_norm(const char *str)
 {
 	if (ft_atof(str) > 1 || ft_atof(str) < -1)
-	{
-		printf("Error\nline %d: Vector format must be [-1.0,1.0]\n", line);
 		return (0);
-	}
 	return (1);
 }
 
@@ -35,7 +32,7 @@ int			ft_check_str_to_vector(const char *str, int norm, int line)
 			break ;
 		if (!ft_check_atof(&str[j], ",", line))
 			break ;
-		if (norm && !ft_check_norm(&str[j], line))
+		if (norm && !ft_check_norm(&str[j]))
 			break ;
 		j = ft_step_to_number(str, j);
 		i++;
