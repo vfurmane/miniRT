3b197697dbe501f00e38e97c3189196dc5887e04
vfurmane <vfurmane@student.42.fr>
2021-03-23 10:04:43 +0100

    Add two messages for status

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:12:46 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/03/23 09:52:04 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/03/23 10:03:41 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,13 @@ int			main(int argc, char **argv)
 		if (ft_check_window_size(scene) == 0)
 			return (1);
 	buffer.fd = -1;
+	printf("Scene parsed successfully!\nProceeding camera(s)...\n");
 	if (!ft_proceed_all_cameras(scene, &buffer, argv))
 	{
 		ft_free_scene(&scene);
 		return (1);
 	}
+	printf("Done!\n");
 	if (scene->mlx != NULL)
 	{
 		ft_proceed_window(scene);
